fd4f58a86e09743010ce466cd701caf6cfd57aa0
Artur Okonski <okonski.a.dev@gmail.com>
2017-03-25 17:10:07 +0100

    Implement #bubble_sort_by method which takes a block

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -12,3 +12,19 @@ def bubble_sort(array)
   end
   sorted_array
 end
+
+def bubble_sort_by(array)
+  sorted_array = array.dup
+  swapped = false
+  array.size.times do |i|
+    array[0..-(i + 2)].each_index do |index|
+      # if sorted_array[index] > sorted_array[index + 1]
+      if yield(sorted_array[index], sorted_array[index + 1]) > 1
+        sorted_array[index], sorted_array[index + 1] = sorted_array[index + 1], sorted_array[index]
+        swapped = true
+      end
+    end
+    break if swapped == false
+  end
+  sorted_array
+end
