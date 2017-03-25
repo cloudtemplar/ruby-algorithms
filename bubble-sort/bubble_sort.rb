3f4f2e46eab4d70ac1694ef4f7c12edc060e07bb
Artur Okonski <okonski.a.dev@gmail.com>
2017-03-25 15:14:44 +0100

    Optimize algorithm performance
    Don't check all of the elements each time- utilise fact
    that after each loop run the number of elements to be checked is
    reduced by one, because they are already sorted.

--- bubble-sort/bubble_sort.rb
+++ bubble-sort/bubble_sort.rb
@@ -1,8 +1,8 @@
 def bubble_sort(array)
   sorted_array = array.dup
   # I could use #each_with_index iterator, but that way code is more readable.
-  array.size.times do
-    array[0..-2].each_index do |index|
+  array.size.times do |i|
+    array[0..-(i + 2)].each_index do |index|
       if sorted_array[index] > sorted_array[index + 1]
         # Swapping elements by parallel assignment.
         sorted_array[index], sorted_array[index + 1] = sorted_array[index + 1], sorted_array[index]
